81540b27446e4872ef16ad7e3c3392dad8ec5d1c
VaishnaviKanipakam <kanipakamvaishnavi1998@gmail.com>
2025-10-14 17:28:08 +0530

    Added new json

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const initializeDbAndServer = async() => {
             console.log("Conected!")
         })  
     } catch(e){
+        console.log(`DB Error: ${e.message}`)
         process.exit(1)
     }
 }
@@ -49,7 +50,7 @@ app.post("/signup", (request, response) => {
     
         db.query(create_user_table, (err, result)=> {
             if(err){
-                response.status(500)
+                response.status(500);
                 return
             }
 
@@ -62,10 +63,10 @@ app.post("/signup", (request, response) => {
 
             db.query(insert_user_details, (err, result) => {
                 if(err){
-                    response.status(500).json("Enter Valid Details")
+                    response.status(500).json("Enter Valid Details");
                     return
                 }
-                response.status(200).json(userDetails)
+                response.status(200).json(userDetails);
             })
     })
         
@@ -90,17 +91,59 @@ app.post("/login", (request, response) => {
         if(err){
             response.status(500).json("Cannot Get Details");
             return
-        }
-        if (result[0].mobile_number === mobileNumber && result[0].password === password){
+        }else if(result.length !== 0 && result[0].mobile_number === mobileNumber && result[0].password === password){
             const payload = {mobileNumber: mobileNumber}
-            const jwtToken = jwt.sign(payload, "oaapmcntholamc")
+            const jwtToken = jwt.sign(payload, "oaapmcntholamc");
             response.status(200).json({result, "token": jwtToken})
+        }else if (result.length == 0){
+             response.status(500).json("Enter Valid Mobile Number and Password")
         }
     })
 })
 
+
+//Authentication Middleware
+const authenticationToken = (request, response, next) => {
+    const authHeader = request.headers["authorization"]
+    let jwtToken;
+    if (authHeader !== undefined) {
+        jwtToken = authHeader.split(" ")[1];
+      }
+      if (jwtToken === undefined) {
+        response.status(401);
+        response.json("183Invalid JWT Token");
+      } else {
+        jwt.verify(jwtToken, "oaapmcntholamc", async (error, payload) => {
+          if (error) {
+            response.status(401);
+            response.json("188Invalid JWT Token");
+          } else {
+            next();
+          }
+        });
+      }
+}
+
+//Get API
+app.get("/get_admin_details", authenticationToken,  (request, response) => {
+    const admin_registration_details = `
+        SELECT
+            *
+        FROM
+            admin_registration;`;
+        db.query(admin_registration_details, (err, result) => {
+            if(err){
+                response.status(500).json("Cannot Get Admin Details")
+            }
+            response.status(200).json(result)
+        })
+})
+
+
 // Post All Categories API
-app.post("/categories", (request, response) => {
+app.post("/categories", authenticationToken, (request, response) => {
+    const categoryDetails = request.body 
+    const {categoryImage, categoryName, categoryCount} = categoryDetails
     const create_category_table = `
         CREATE TABLE IF NOT EXISTS category_table (
             category_id  INTEGER NOT NULL AUTO_INCREMENT,
@@ -112,7 +155,7 @@ app.post("/categories", (request, response) => {
 
     db.query(create_category_table, (err, result) => {
         if(err){
-            response.status(500)
+            response.status(500);
             return
         }
            
@@ -120,29 +163,23 @@ app.post("/categories", (request, response) => {
             INSERT INTO 
                 category_table (category_image, category_name, item_count)
             VALUES 
-                ("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS49X_cSUrau_6WBf33JqdMS8LeXYvR0SMMCu_-xruWEtEOonAMdiETkm5-O-POJ2gVV0&usqp=CAU", "Men Clothes", 24),
-                ("https://img.freepik.com/free-photo/front-view-woman-wearing-hat_23-2149726075.jpg?semt=ais_hybrid&w=740", "Women Clothes", 12),
-                ("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgaRpxuCEnhHDo_xKGcundMmPk4VxHzIVZoA&s", "Accessories", 43),
-                ("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWTZoyOYHHdmLpeKgHNXWbsjcb0nFfk85jxA&s", "Cotton Clothes", 31),
-                ("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbIkH4d9IPA4seGezbYrulQl5m7pUbWrDfBQ&s", "Summer Clothes", 26),
-                ("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkKoM8Rx38A8zr0YJ8lrRTz836IW1Ra-k0Sw&s", "Wedding Clothes", 52),
-                ("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeW0ezCkdz8fYxWGZKDoQrPTPSIlbzjYl4pg&s", "Spring Colletion", 30),
-                ("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSc0XGX9D4CQ0XJBtUgkj2xjxa8uSvCTw1gcQ&s", "Casual Clothes", 52),
-                ("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGPgxzMQSinGi4x-bSntnGllwhg1UCNuUbDw&s", "Hats", 47);
+                ("${categoryImage}", "${categoryName}", ${categoryCount});
         `;
         db.query(insert_data_into_category_table, (err, result) => {
             if(err){
-                response.status(500).json("Cannot Update Category")
+                response.status(500).json("Cannot Update Category");
                 return
             }
-            response.status(200).json("Categories Insereted Successfully")
+            response.status(200).json("Categories Insereted Successfully");
         })
     })
 })
 
 
+
+
 // Get All Categories API
-app.get("/get_category", (request, response) => {
+app.get("/get_category", authenticationToken, (request, response) => {
     const get_data_from_category_table = `
         SELECT
             *
@@ -151,9 +188,126 @@ app.get("/get_category", (request, response) => {
     `;
     db.query(get_data_from_category_table, (err, result) => {
         if(err){
-            response.status(500).json("Something Went Wrong")
+            response.status(500).json("Something Went Wrong");
             return
         }
-        response.status(200).json(result)
+        response.status(200).json(result);
     })
 })
+
+
+// Edit Category API
+app.put("/edit_category", authenticationToken, (request, response) => {
+    const categoryId = request.query.category_id
+    const editCategoryDetails = request.body
+    const {editCategoryImage, editCategoryName, editCategoryCount} = editCategoryDetails;
+    
+    const update_category_query = `
+        UPDATE
+            category_table
+        SET
+            category_image = "${editCategoryImage}", 
+            category_name = "${editCategoryName}", 
+            item_count = ${editCategoryCount}
+        WHERE 
+            category_id = ${categoryId};`;
+
+    db.query(update_category_query, (err, result) => {
+        if(err){
+            response.status(500).json(`Cannot Update ${editCategoryName}`)
+            return
+        }
+        response.status(200).json(`${editCategoryName} Updated Successfully`)
+    })
+})
+
+
+// Category Detailed API
+app.post("/add_category_type", authenticationToken, (request, response) => {
+    const categoryId = request.query.category_id
+    const addCategoryTypeDetails = request.body
+    const {addCategoryType, addCategoryImage, addCategoryName, addCategoryCount} = addCategoryTypeDetails
+    const create_category_Details_table = `
+         CREATE TABLE IF NOT EXISTS category_type_table (
+            each_category_id  INTEGER NOT NULL AUTO_INCREMENT,
+            category_type VARCHAR (1000),
+            category_image VARCHAR (1000),
+            category_name VARCHAR (1000),
+            item_count INTEGER,
+            category_id INTEGER,
+            PRIMARY KEY (each_category_id),
+            FOREIGN KEY (category_id) REFERENCES category_table(category_id));`;
+
+    db.query(create_category_Details_table, (err, result) => {
+        if(err){
+            response.status(500).json("Cannot Create Table")
+            return
+        }
+
+            const insert_data_into_category_Details_table= `
+                INSERT INTO 
+                    category_type_table (category_id, category_type, category_image, category_name,  item_count)
+                VALUES
+                (${categoryId}, "${addCategoryType}", "${addCategoryImage}", "${addCategoryName}", ${addCategoryCount});
+            `;
+    db.query(insert_data_into_category_Details_table, (err, result) => {
+        if(err){
+            response.status(500).json(`Cannot Insert Data`);
+            return
+        }
+        response.status(200).json("Data Inserted Successfully");
+    })
+        
+    })
+})
+
+
+// Get Category Detailes API
+app.get("/get_each_category_list", authenticationToken, (request, response) => {
+    const categoryId = request.query.category_id
+    const get_category_Details_table_data = `
+        SELECT 
+            *
+        FROM 
+            category_type_table
+        WHERE
+            category_id = ${categoryId};`;
+
+    db.query(get_category_Details_table_data, (err, result) => {
+        if(err){
+            response.status(500).json("Cannot Get Data")
+            return
+        }
+        response.status(200).json(result);
+    })
+})
+
+
+// Edit Category Detailes API 
+app.put("/edit_catgory_type", authenticationToken, (request, response) => {
+    const eachCategoryId = request.query.each_category_type_id;
+    const editCategoryTypeDetails = request.body;
+    const {editCategoryTypeImage, editCategoryTypeName, editCategoryTypeCount, editCategoryType} = editCategoryTypeDetails
+
+    const  update_category_type_query = `
+        UPDATE
+            category_type_table
+        SET
+            category_image = "${editCategoryTypeImage}",
+            category_name = "${editCategoryTypeName}",
+            item_count = "${editCategoryTypeCount}",
+            category_type = "${editCategoryType}"
+        WHERE 
+            each_category_id = ${eachCategoryId};
+
+    `;
+    db.query(update_category_type_query, (err, result) => {
+        if(err){
+            response.status(500).json("Cannot Update Category")
+            return
+        }
+        response.status(200).json("Category Updated Successfully")
+    })
+
+})
+
